056678dae6e9fa48fc3c2e19980dfbfe923d2c4e
Julian <julianser@hotmail.com.ar>
2023-11-27 19:55:54 -0300

    Se agrego un logo y color al navbar

--- src/common/Menu.jsx
+++ src/common/Menu.jsx
@@ -1,12 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import LogoAmiPet from '../assets/AmiPet-logo.png'
 
 const Menu = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="Navbar">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={LogoAmiPet} className='logo'></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
