00143b7d83500923f8c0cdae9592a3342d9ffed3
Julian <julianser@hotmail.com.ar>
2024-01-25 16:56:38 -0300

    Se realizo la pagina detalle

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -4,14 +4,16 @@ const Detail = () => {
   return (
     <Container className="mainSection mt-5">
         <h2 className="text-center">Informacion de Pepito</h2>   
-        <Card>
+        <Card className="border-3 rounded border-black">
           <Card.Body>
             <Row>
               <Col md={6}>
-                <img className="w-75" src={ImgPrueba}></img>
+                <img className="imagenDetalle w-100" src={ImgPrueba}></img>
               </Col>
               <Col md={6}>
-                Ayuda a encontrar a Pepito, se perdio el 20 de enero de 2024
+                <h2>Ayuda a encontrar a Pepito</h2>
+                <hr></hr>
+                <h5>Desaparecio el 20 de enero de 2024, tiene una mancha negra en su pecho además lleva un collar</h5>
               </Col>
             </Row>
           </Card.Body>
